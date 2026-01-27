b2f3b82e84860fd616fd3c868a3be22853aa4d88
SalvadorAbeja <salvador.abeja@campus.mccd.edu>
2026-01-27 10:18:13 -0800

    Update Dog.java

--- Dog.java
+++ Dog.java
@@ -10,5 +10,16 @@ public class Dog {
     public String name;
     public int age;
     public String breed;
+    public Dog() {            // default constructor
+    name = "Unknown";
+    age = 0;
+    breed = "Mixed";
+}
+
+public Dog(String name, int age, String breed) { // parameterized constructor
+    this.name = name;
+    this.age = age;
+    this.breed = breed;
+}
 }
 
